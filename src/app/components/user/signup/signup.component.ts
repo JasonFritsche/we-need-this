5221b96cd8237092978f63d5ff11283ea2a4677d
Jason Fritsche <fritsche.jason@gmail.com>
2022-05-18 22:36:38 -0500

    added password special characters validation

--- src/app/components/user/signup/signup.component.ts
+++ src/app/components/user/signup/signup.component.ts
@@ -28,18 +28,15 @@ export class SignupComponent implements OnInit {
         Validators.required,
         Validators.minLength(8),
         Validators.maxLength(40),
+        this.passwordStrength,
       ]),
     ],
     verifyPassword: ['', [Validators.required, this.comparePassword]],
   });
 
-  ngOnInit(): void {
-    this.signUpForm.valueChanges.subscribe((change) => {
-      console.log(this.signUpForm);
-    });
-  }
+  ngOnInit(): void {}
 
-  //convenience getter for easy access to form fields
+  // Convenience getter for easy access to form fields
   get form(): { [key: string]: AbstractControl } {
     return this.signUpForm.controls;
   }
@@ -48,12 +45,29 @@ export class SignupComponent implements OnInit {
     this.userStore.dispatch(SignUp({ payload: this.signUpForm.value }));
   }
 
+  // Validator that compares password entry with verify password entry
   comparePassword(control: AbstractControl): ValidationErrors | null {
     if (
       control.parent?.get('password')?.value !==
       control.parent?.get('verifyPassword')?.value
     ) {
       return { passwordMatchError: { value: true } };
+    }
+    return null;
+  }
+
+  // Validator that ensure password meets password strength criteria (1 uppercase, 1 lowercase, 1 number, 1 special character)
+  passwordStrength(control: AbstractControl): ValidationErrors | null {
+    let hasNumber = /\d/.test(control.parent?.get('password')?.value);
+    let hasUpper = /[A-Z]/.test(control.parent?.get('password')?.value);
+    let hasLower = /[a-z]/.test(control.parent?.get('password')?.value);
+    let hasSpecialCharacter = /[!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?]/.test(
+      control.parent?.get('password')?.value
+    );
+    const valid = hasNumber && hasUpper && hasLower && hasSpecialCharacter;
+
+    if (!valid) {
+      return { passwordStrengthError: { value: true } };
     }
     return null;
   }
