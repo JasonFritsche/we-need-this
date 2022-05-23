1ab26db99a71a5e7eb8b057e7e990667226372b8
Jason Fritsche <fritsche.jason@gmail.com>
2022-05-22 19:58:09 -0500

    fix: fixed failing unit tests

--- src/app/components/user/signup/signup-form/signup-form.component.spec.ts
+++ src/app/components/user/signup/signup-form/signup-form.component.spec.ts
@@ -1,6 +1,16 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { StoreModule } from '@ngrx/store';
 
+import { ContainerComponent } from 'src/app/modules/shared/components/container/container.component';
 import { SignupFormComponent } from './signup-form.component';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatIconTestingModule } from '@angular/material/icon/testing';
+import { MockComponent } from 'ng-mocks';
 
 describe('SignupFormComponent', () => {
   let component: SignupFormComponent;
@@ -8,9 +18,19 @@ describe('SignupFormComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ SignupFormComponent ]
-    })
-    .compileComponents();
+      declarations: [SignupFormComponent, MockComponent(ContainerComponent)],
+      imports: [
+        MatIconTestingModule,
+        BrowserAnimationsModule,
+        StoreModule.forRoot({}),
+        FormsModule,
+        ReactiveFormsModule,
+        MatFormFieldModule,
+        MatInputModule,
+        MatButtonModule,
+        MatIconModule,
+      ],
+    }).compileComponents();
   });
 
   beforeEach(() => {
@@ -21,5 +41,15 @@ describe('SignupFormComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should require valid email', () => {
+    component.signUpForm.setValue({
+      email: 'invalidemail',
+      password: 'Qwerty@1',
+      verifyPassword: 'Qwerty@1',
+    });
+
+    expect(component.signUpForm.valid).toEqual(false);
   });
 });
